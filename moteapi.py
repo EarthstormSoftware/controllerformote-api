72629e727381142dac6b53615bf77f9b353cec5e
David Glaude <david.glaude@gmail.com>
2017-04-01 22:20:48 +0200

    Was making error in python3 because / return float. For python3 and python2 compatibility // return int in both.

--- moteapi.py
+++ moteapi.py
@@ -16,7 +16,7 @@ status = 0
 def hex_to_rgb(value):
     value = value.lstrip('#')
     length = len(value)
-    return tuple(int(value[i:i + length / 3], 16) for i in range(0, length, length / 3))
+    return tuple(int(value[i:i + length // 3], 16) for i in range(0, length, length // 3))
 
 def mote_on(c):
     r, g, b = hex_to_rgb(c)
